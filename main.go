62d5c39254e96fe3f334939b2c08df0cf1bb13ae
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2023-01-21 23:31:50 +0200

    fixed create and update logic

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Movie struct {
-	ID string `json:"id`
+	ID string `json:"id"`
 	Isbn string `json:"isbn"`
 	Title string `json:"title"`
 	Director *Director `json:"director"`
@@ -32,21 +32,26 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-
 	for _, item := range movies {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(1000000000))
+	
+	params := mux.Vars(r)
+	if (params["id"] == "") {
+		movie.ID = strconv.Itoa(rand.Intn(1000000000))
+	} else {
+		movie.ID = params["id"]
+	}
+	
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
